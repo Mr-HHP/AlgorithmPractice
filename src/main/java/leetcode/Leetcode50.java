681f5dcee1d60c700646c7116c6c95f996904f80
haipeng.huang@mail.hypers.com <2313117162@qq.com>
2022-03-28 13:57:59 +0800

    新增：Leetcode算法题

--- src/main/java/leetcode/Leetcode50.java
+++ src/main/java/leetcode/Leetcode50.java
@@ -7,26 +7,37 @@ package leetcode;
  * @date 2022/03/26
  **/
 public class Leetcode50 {
+    /**
+     * 利用快速幂+循环解题（基于分治法的思想）
+     * @param x 给定数字
+     * @param n 幂
+     * @return x的n次幂
+     */
     public double myPow(double x, int n) {
-        if (n == 0 || x == 1.0) {
-            return 1;
-        }
-        double result = 1;
-        for (int i = 0; i < Math.abs((long) n); i++) {
-            result = result * x;
-            if (x > 10000) {
-                return 10000;
+        return (long) n >= 0 ? quickMul(x, (long) n) : 1.0 / quickMul(x, -(long) n);
+    }
+    
+    public double quickMul(double x, long n) {
+        double ans = 1.0;
+        // 贡献的初始值为 x
+        double xContribute = x;
+        // 在对 n 进行二进制拆分的同时计算答案
+        while (n > 0) {
+            if (n % 2 == 1) {
+                // 如果 n 二进制表示的最低位为 1，那么需要计入贡献
+                ans = ans * xContribute;
             }
-            if (x < -10000) {
-                return -10000;
-            }
+            // 将贡献不断地平方
+            xContribute = xContribute * xContribute;
+            // 舍弃 n 二进制表示的最低位，这样我们每次只要判断最低位即可
+            n = n >> 1;
         }
-        return n > 0 ? result : 1 / result;
+        return ans;
     }
     
     public static void main(String[] args) {
         Leetcode50 leetcode50 = new Leetcode50();
-        double myPow = leetcode50.myPow(2, 10);
+        double myPow = leetcode50.myPow(10, 0);
         System.out.println(myPow);
     }
 }
