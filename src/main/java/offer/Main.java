c4981089b45066a352e560f628fb2562fc7d29ce
haipeng.huang@mail.hypers.com <2313117162@qq.com>
2022-04-05 01:19:38 +0800

    新增：华为OD笔试题

---
+++ src/main/java/offer/Main.java
@@ -0,0 +1,59 @@
+package offer;
+
+import java.text.FieldPosition;
+import java.util.*;
+
+/**
+ * @author Mr.黄
+ * @date 2022/04/04
+ **/
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] vlanPool = in.nextLine().split(",");
+        int needVlan = in.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (String vlan : vlanPool) {
+            String[] split = vlan.split("-");
+            if (split.length > 1) {
+                for (int i = Integer.valueOf(split[0]); i <= Integer.valueOf(split[1]); i++) {
+                    System.out.println(i);
+                    list.add(i);
+                }
+            } else {
+                list.add(Integer.parseInt(split[0]));
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (needVlan == list.get(i)) {
+                list.remove(i);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        Collections.sort(list);
+        int i = 0;
+        while (i < list.size()) {
+            int j = 1;
+            int first = list.get(i);
+            while (j <= list.size() - 1 - i) {
+                if (list.get(i + j) == list.get(i) + j) {
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            if (j == 1) {
+                result.append(first).append(",");
+                i++;
+            } else {
+                result.append(first).append("-").append((first + j - 1)).append(",");
+                i = i + j;
+            }
+        }
+        if (result.length() == 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(result.toString().substring(0, result.length() - 1));
+        }
+    }
+}
--- src/main/java/offer/RunMain.java
+++ src/main/java/offer/RunMain.java
@@ -1,7 +1,6 @@
 package offer;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 运行主类
@@ -10,7 +9,57 @@ import java.util.Map;
  * @date 2021/10/10
  **/
 public class RunMain {
-  public static void main(String[] args) {
-  
-  }
+    public static void main(String[] args) {
+//        main1(null);
+//        System.out.println((int) 'a');
+//        System.out.println((int) 'z');
+        System.out.println(getNum(47));
+        System.out.println(getNum(48));
+        System.out.println(getNum(49));
+        System.out.println((97 + (122 - 97) % 26));
+        System.out.println((97 + (123 - 97) % 26));
+        System.out.println((97 + (124 - 97) % 26));
+        System.out.println((97 + (200 - 97) % 26));
+        System.out.println((97 + (300 - 97) % 26));
+    }
+    public static void main1(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+            char[] chars = str.toCharArray();
+            for (int j = 0; j < str.length(); j++) {
+                long num = chars[j] + getNum(j);
+                if (num >= 97 && num <= 122) {
+                    chars[j] = (char) (num);
+                } else {
+                    chars[j] = (char) ((num - 'a') % 26 + 'a');
+                }
+            }
+            result.add(String.valueOf(chars));
+        }
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+    
+    private static long getNum(int num) {
+        long a1 = 1, a2 = 2, a3 = 4, a4 = 0;
+        if (num == 0) {
+            return a1;
+        } else if (num == 1) {
+            return a2;
+        } else if (num == 2) {
+            return a3;
+        } else {
+            for (int i = 3; i <= num; i++) {
+                a4 = a1 + a2 + a3;
+                a1 = a2;
+                a2 = a3;
+                a3 = a4;
+            }
+            return a4;
+        }
+    }
 }
