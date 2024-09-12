cb92b4bf3fcf583315a64191f467b94f73b6b6e4
Mr.黄 <2313117162@qq.com>
2024-09-12 09:33:40 +0800

    新增: 朋友圈并查集算法题

--- src/main/java/test/Main2.java
+++ src/main/java/test/Main2.java
@@ -1,8 +1,72 @@
-package test;/**
-*
-* @description 
-* @author Mr.黄
-* @since 2024/7/25 22:50
-**/
+package test;
+
+import java.util.*;
+import java.util.Scanner;
+
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
 public class Main2 {
+    /*
+    朋友圈
+班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有传递性。
+如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。
+所谓的朋友圈，是指所有朋友的集合。
+给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。
+如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。
+你必须输出所有学生中的已知的朋友圈总数。
+     */
+    /*
+    示例 1:
+输入:
+[[1,1,0],
+ [1,1,0],
+ [0,0,1]]
+输出: 2
+说明：已知学生0和学生1互为朋友，他们在一个朋友圈。
+第2个学生自己在一个朋友圈，所以返回2。
+     */
+    /*
+    示例 2:
+输入:
+[ [1,1,0],
+ [1,1,1],
+ [0,1,1] ]
+输出: 1
+说明：已知学生0和学生1互为朋友，学生1和学生2互为朋友，所以学生0和学生2也是朋友，所以他们三个在一个朋友圈，返回1。
+注意：
+N 在[1,200]的范围内。
+对于所有学生，有M[i][i] = 1。
+如果有M[i][j] = 1，则有M[j][i] = 1。
+     */
+    public static void main(String[] args) {
+        int[][] m = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        int[] parens = new int[m.length];
+        for (int i = 0; i < parens.length; i++) {
+            parens[i] = i;
+        }
+        int count = m.length;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < m.length; i++) {
+            for (int j = i + 1; j < m[0].length; j++) {
+                if (m[i][j] == 1 && help(parens, i, j)) {
+                    count--;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+    public static int isRoot(int[] parens, int i) {
+        if (parens[i] != i) parens[i] = isRoot(parens, parens[i]);
+        return parens[i];
+    }
+    public static boolean help(int[] parens, int i, int j) {
+        int rootI = isRoot(parens, i);
+        int rootJ = isRoot(parens, j);
+        if (rootJ != rootI) {
+            parens[rootI] = rootJ;
+            return true;
+        }
+        return false;
+    }
+
+
 }
